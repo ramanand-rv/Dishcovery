4e51c6b603c5455b95da1f6a8ac87a12bbea6ffd
Ramanand <ramanandt35@gmail.com>
2024-04-04 23:47:58 +0530

    featured added

---
+++ components/Featured.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Featured = () => {
+  return (
+    <View>
+      <Text>Featured</Text>
+    </View>
+  );
+};
+
+export default Featured;
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -2,6 +2,7 @@ import {View, ScrollView} from 'react-native';
 import React from 'react';
 import Header from '../components/Header';
 import Categories from '../components/Categories';
+import Featured from '../components/Featured';
 
 const HomeScreen = () => {
   return (
@@ -11,15 +12,8 @@ const HomeScreen = () => {
         {/* Categories */}
         <Categories />
 
-
-
-
         {/* Featured */}
-
-
-
-
-        
+        <Featured />
       </ScrollView>
     </View>
   );
